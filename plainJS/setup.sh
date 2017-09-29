1378c33fb24d2273e31eb54849a4a84d170c3966
Lucas Hawes <doubletigerstripe@gmail.com>
2017-09-28 17:24:00 -0700

    changed and moved setup to setup everything

--- plainJS/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-nvm install 7
-nvm use 7
-
-npm install
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+npm install -g mocha chai
+cd plainJS
+npm install && cd ../react
+npm install && cd ../
